9a60e3d08b54dfbb7d9c4efafa28714ac4f2291f
Sõnia Iraci Conceiçao de Freitas <soniaicf@outlook.com.br>
2021-02-03 18:57:06 -0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+let dateNow = new Date()
+let dateEnd = new Date("2021/07/07")
+let MSLeft = dateEnd - dateNow
+let seconds = MSLeft / 1000
+let minutes = seconds / 60
+let hours = minutes / 60
+let days = Math.floor(hours / 24)
+let s = (hours / 24) - days
+
+let elDays = document.getElementById("days")
+
+console.log(elDays)
+elDays.innerHTML = days
